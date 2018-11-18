e3a5f1724918b145c6fbeb8a507a26985b5c22fc
Анастасия Нестерюкова <nestery-nastya@mail.ru>
2018-11-18 22:59:17 +0600

    Updates

---
+++ precache-manifest.649688dde113bae53e6fbce9ca153269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d1469ea60f852e911f",
+    "url": "/pomodoro-clock-fcc/static/js/runtime~main.46d1469e.js"
+  },
+  {
+    "revision": "4ffeb00459791deccc0e",
+    "url": "/pomodoro-clock-fcc/static/js/main.4ffeb004.chunk.js"
+  },
+  {
+    "revision": "c8d6c40be6eebf976d45",
+    "url": "/pomodoro-clock-fcc/static/js/1.c8d6c40b.chunk.js"
+  },
+  {
+    "revision": "4ffeb00459791deccc0e",
+    "url": "/pomodoro-clock-fcc/static/css/main.dfaa3d84.chunk.css"
+  },
+  {
+    "revision": "0dc4a907c1b39f60f883afe24bee4587",
+    "url": "/pomodoro-clock-fcc/index.html"
+  }
+];
